4ce086ffb966ba9432a8a3a0e2565f3b6bd46507
ChrisMuteb <ibond.chris@gmail.com>
2022-01-02 19:51:16 +0200

    server ready for connection
    The server is now waiting for the multiple connection on the port number specified in the argument passed at genesis; while the password is also authenticated

--- run_app.c
+++ run_app.c
@@ -1,22 +1,98 @@
-#include <unistd.h>
+/* echo_server_thread.c
+ *
+ * Copyright (c) 2000 Sean Walton and Macmillan Publishers.  Use may be in
+ * whole or in part in accordance to the General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+*/
 #include <stdlib.h>
-#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdio.h>
 
+/*Definition*/
+#define DEFAULT_BUFLEN 1024
+#define PORT 8200
+
+void PANIC(char* msg);
+#define PANIC(msg)  { perror(msg); exit(-1); }
+void* Child(void* arg);
+
+//----------------------------------------------
 void error_message();
 int password_correct(char *pswdFile, char *pswd);
-void init(int argc, char *argv[]);
+void init(int argc, char *argv[], int *port_num);
 
 int main(int argc, char *argv[]){
 
-    init(argc, argv);
+    int sd,opt,optval;
+    struct sockaddr_in addr;
+    //unsigned short port=0;
+    int p_num = 0;//port num from the cmd argument
+
+
+    init(argc, argv, &p_num);//get the 3 arguments and return the port number
+
+
+    if ( (sd = socket(PF_INET, SOCK_STREAM, 0)) < 0 )
+        PANIC("Socket");
+    addr.sin_family = AF_INET;
+
+    if ( p_num > 0 )
+                addr.sin_port = htons(p_num);
+    else
+                addr.sin_port = htons(PORT);
+
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+   // set SO_REUSEADDR on a socket to true (1):
+   optval = 1;
+   setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval);
+
+
+    if ( bind(sd, (struct sockaddr*)&addr, sizeof(addr)) != 0 )
+        PANIC("Bind");
+    if ( listen(sd, SOMAXCONN) != 0 )
+        PANIC("Listen");
+
+    printf("System ready on port %d\n",ntohs(addr.sin_port));
+
+    while (1)
+    {
+        int client, addr_size = sizeof(addr);
+        pthread_t child;
+
+        client = accept(sd, (struct sockaddr*)&addr, &addr_size);
+        printf("Connected: %s:%d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+        if ( pthread_create(&child, NULL, Child, &client) != 0 )
+            perror("Thread creation");
+        else
+            pthread_detach(child);  /* disassociate from parent */
+    }
+
     
 
     exit(EXIT_SUCCESS);
 }
 void error_message(){
     printf("Required: -d <directory> | -p <port number> | -u <password>\n");
-    exit(2);
+    exit(1);
 }
 int password_correct(char *pswdFile, char *pswd){
 
@@ -52,13 +128,13 @@ int password_correct(char *pswdFile, char *pswd){
     return ispswdFound;
 }
 
-void init(int argc, char *argv[]){
+void init(int argc, char *argv[], int *port_num){
     if(argc < 6){//ensures that the number of arguments is not less than 4
         error_message();
     }
     
     int optionInput;//Getopt var. holder
-    char *directory, *port_num, *password;
+    char *directory, *password;
     
     
     while((optionInput = getopt(argc, argv, "d:p:u:")) != -1){//prompt the user for the input and output file
@@ -69,7 +145,7 @@ void init(int argc, char *argv[]){
             directory = optarg;
             break;
         case 'p':
-            port_num = optarg;
+            *port_num = atoi(optarg);
             break;
         case 'u':
             password = optarg;
@@ -82,11 +158,43 @@ void init(int argc, char *argv[]){
     }
     printf("running dir: %s\n", directory);
 
-    printf("port num: %s\n", port_num);
-    printf("password: %s\n", password);
+    printf("Port num: %d\n", *port_num);
+    printf("Password: %s\n", password);
 
     puts("");
     if(password_correct("password.txt", password)){
         puts("password found!");
     }
 }
+
+
+/*--------------------------------------------------------------------*/
+/*--- Child - echo server                                         ---*/
+/*--------------------------------------------------------------------*/
+void* Child(void* arg)
+{   char line[DEFAULT_BUFLEN];
+    int bytes_read;
+    int client = *(int *)arg;
+
+    do
+    {
+        bytes_read = recv(client, line, sizeof(line), 0);
+        if (bytes_read > 0) {
+                if ( (bytes_read=send(client, line, bytes_read, 0)) < 0 ) {
+                        printf("Send failed\n");
+                        break;
+                }
+        } else if (bytes_read == 0 ) {
+                printf("Connection closed by client\n");
+                break;
+        } else {
+                printf("Connection has problem\n");
+                break;
+        }
+    } while (bytes_read > 0);
+    close(client);
+    return arg;
+}
+
+
+
