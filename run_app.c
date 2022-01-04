315ffc83c81619d784397e4c34d7fb4407d27b4c
ChrisMuteb <ibond.chris@gmail.com>
2022-01-05 00:45:09 +0200

    LIST cmd
    The list command has been added to the project. And can only be executed once you have successfully logged in the system. The list of all files in the directory are displayed, however, the size will be implemented a bit later on

--- run_app.c
+++ run_app.c
@@ -41,11 +41,13 @@ int password_correct(char *pswdFile, char *pswd);
 void init(int argc, char *argv[]);
 int authenticateUSER(char *pswdFile, char u[20], char p[20]);
 void listDirectories();
+void trimRecvbuf(char *s);
 
 char u_name[20];//stores the username
 
 int client, port_num;
 char p_file[20], d_file[20];
+
 char passwordFile[20] = "password.txt";
 
 int main(int argc, char *argv[]){
@@ -173,27 +175,25 @@ void listDirectories(){
     
     DIR *d;
     struct dirent *dir;
-    d = opendir(".");
-    int bytes_r = 0;
-    char str[40] = {0};
+    d = opendir(d_file);//opendir(".");
+    int bytes_r = 0, len;
+    char str[DEFAULT_BUFLEN] = {0}, lenStr[5];
 
     if (d)
     {
         while ((dir = readdir(d)) != NULL)
         {
-            printf("%s %ld\n", dir->d_name, strlen(dir->d_name));
+            printf("%s %ld\n", dir->d_name, sizeof(dir->d_name));
             //----------------
             strcpy(str, dir->d_name);
-            strcat(str, " ");
-            strcat(str, (char *)strlen(dir->d_name));
-            if(bytes_r <= 0)
-            bytes_r = 40;
-
-            while(bytes_r > 0){
+            strcat(str, "\n");
+            //len = sizeof(dir->d_name);
+            //display the size also
+            //strcat(str, lenStr);
+            bytes_r = strlen(str);
+            if(bytes_r > 0)
                 send(client, str, bytes_r, 0);
-                break;
-            }
-            puts("\n");
+            
             
         }
 
@@ -238,9 +238,9 @@ int authenticateUSER(char *pswdFile, char u[20], char p[20]){
 void* Child(void* arg)
 {   
     char line[DEFAULT_BUFLEN] = {0};
-    int bytes_read;
+    int bytes_read, is_authen = 0;
     client = *(int *)arg;
-    char cmd_USER[20] = {0}, cmd_uname[20] = {0}, cmd_pswd[20] = {0}, cmd_LIST[5] = {0};
+    char cmd_USER[20] = {0}, cmd_uname[20] = {0}, cmd_pswd[20] = {0}, cmd_LIST[20] = {0};
     char str_USER[70] = {0};
     //-----------Welcoming message sent to user-------------
     char str[35] = {0};
@@ -248,6 +248,9 @@ void* Child(void* arg)
     strcat(str, "Welcome to Chris\'s file server\n");
     send(client, str, bytes_r, 0);
     //--------------------------
+    int rcnt = 0;
+    char recvbuf[DEFAULT_BUFLEN]; 
+    int recvbuflen = DEFAULT_BUFLEN;
     
     do
     {
@@ -267,6 +270,7 @@ void* Child(void* arg)
                     strcpy(str_USER, "\n200 User ");
                     strcat(str_USER, cmd_uname);
                     strcat(str_USER, " granted to access.\n");
+                    is_authen = 1;//set to true
 
                     if(bytes_r <= 0)
                     bytes_r = 35;
@@ -303,17 +307,61 @@ void* Child(void* arg)
         }
         
         //-----------------
-        bytes_read = recv(client, cmd_LIST, 5, 0);
+        /*bytes_read = recv(client, cmd_LIST, 5, 0);
         strcpy(cmd_LIST, strtok(cmd_LIST, " "));
         printf("nOW: %s-%ld\n", cmd_LIST, strlen(cmd_LIST));
         int i = strcmp(cmd_LIST, "LIST");
         printf("list function called %d\n", i);
         if(strcmp(line, cmd_LIST)){
-            //puts("list function called");
+            puts("list function called");
+        }*/
+
+        //-------------------------
+        if(is_authen){
+            rcnt = recv(client, recvbuf, recvbuflen, 0);
+            char lst[recvbuflen];
+            strcpy(lst,"LIST");
+            trimRecvbuf(recvbuf);
+            //printf("\n%ld-%ld=>%c*%c\n", strlen(lst), strlen(recvbuf), recvbuf[4], recvbuf[5]);
+            //trim the str from client
+            int iseq = 0;
+            for(int i = 0; i < 4; i++){
+                if(recvbuf[i] == lst[i])
+                    iseq = 1;
+                else 
+                    iseq = 0;
+            }
+           //printf("\n%d\n", iseq);
+            if (rcnt > 0 && (iseq == 1)) {//user entered LIST
+                listDirectories();
+                printf("Bytes received: %d\n", rcnt);
+            // Echo the buffer back to the sender
+                rcnt = send( client, recvbuf, rcnt, 0 );
+            if (rcnt < 0) {
+                printf("Send failed:\n");
+                close(client);
+                break;
+            }
+            printf("Bytes sent: %d\n", rcnt);
+
         }
+        }
+        
+        
+
+        
+        //-----------------------------
     } while (bytes_read > 0);
     close(client);
     return arg;
+}
+void trimRecvbuf(char *s){
+    int i = strlen(s) - 1;
+    while(i > 0){
+        if(s[i] == '\a' || s[i] == '\b' || s[i] == '\f' || s[i] == '\v' || s[i] == ' ' || s[i] == '\n' || s[i] == '\t' || s[i] == '\r') i--;
+        else break;
+    }
+    s[i+1] = '\0';
 }
 
 
